78b29d8858669619f547555a919d61cf5d61c5d3
Mohamork <k.m19981102@gmail.com>
2026-01-20 16:55:24 +0100

    Fix spelling

--- app/main/models.py
+++ app/main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Book() :
-    author = models.CharField(max_lenght=50)
+    author = models.CharField(max_length=50)
     title = models.CharField(max_length=70)
-    
+
 
