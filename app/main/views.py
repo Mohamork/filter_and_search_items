7d3d1c244d67e1867847e2df5723d9c6b01f1b7e
Mohamork <k.m19981102@gmail.com>
2026-01-20 17:16:28 +0100

    Render input form

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpRedirect
+from .models import Book
+from .forms import BookForm
 
 # Create your views here.
+
+def get_input(request) :
+    if request.method == 'POST' :
+        form = BookForm(request.POST)
+        if form.is_valid() :
+            form.save()
+            return HttpRedirect('result')
+    else:
+        form = BookForm
+        return render(request,input.html,{'form':form})
+
